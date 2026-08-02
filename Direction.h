246d360a5d3591f72fa722323f4c20af1fef4b9f
MayaStudentPwr <281160@student.pwr.edu.pl>
2026-08-02 16:51:17 +0200

    Point stuct implementation

--- Direction.h
+++ Direction.h
@@ -24,3 +24,29 @@ public:
         return Direction{ random };
     }
 };
+
+struct Point{
+    int x{};
+    int y{};
+
+    friend bool operator==(const Point& p1, const Point& p2)
+    {
+        return (p1.x == p2.x && p1.y == p2.y);
+    }
+    
+    friend bool operator!=(const Point& p1, const Point& p2)
+    {
+        return !(operator==(p1, p2));
+    }
+
+    const Point& getAdjacentPoint(Direction dir) {
+        switch (dir.getType())
+        {
+        case Direction::up:     return Point{ x,     y - 1 };
+        case Direction::down:   return Point{ x,     y + 1 };
+        case Direction::left:   return Point{ x - 1, y };
+        case Direction::right:  return Point{ x + 1, y };
+        default:                break;
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 #include <iostream>
 
 int main(){
-    UserInput::App_Step5(); 
-    
+    std::cout << std::boolalpha;
+    std::cout << (Point{ 1, 1 }.getAdjacentPoint(Direction::up)    == Point{ 1, 0 }) << '\n';
+    std::cout << (Point{ 1, 1 }.getAdjacentPoint(Direction::down)  == Point{ 1, 2 }) << '\n';
+    std::cout << (Point{ 1, 1 }.getAdjacentPoint(Direction::left)  == Point{ 0, 1 }) << '\n';
+    std::cout << (Point{ 1, 1 }.getAdjacentPoint(Direction::right) == Point{ 2, 1 }) << '\n';
+    std::cout << (Point{ 1, 1 } != Point{ 2, 1 }) << '\n';
+    std::cout << (Point{ 1, 1 } != Point{ 1, 2 }) << '\n';
+    std::cout << !(Point{ 1, 1 } != Point{ 1, 1 }) << '\n';
+
     return 0;
 }
